0efadde315da077028dd429f276977562b13ad10
Dung Nguyen <nndung179@gmail.com>
2016-06-29 00:51:50 +0700

    add bootstrap file for node_module

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = undefined;
+
+var _SwitchToggle = require('./src/components/SwitchToggle/SwitchToggle');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _SwitchToggle.default;
